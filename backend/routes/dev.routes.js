c13abbecd9ba43dacb2f67893c9b2635965becaf
Sneha <231210101@nitdelhi.ac.in>
2025-09-20 00:57:42 +0530

    deleted redundant files and folders

--- backend/routes/dev.routes.js
+++
@@ -1,9 +0,0 @@
-// File: routes/dev.routes.js
-const express = require('express');
-const router = express.Router();
-const { requireAuth } = require('../middleware/auth');
-const ctrl = require('../controllers/dev.controller');
-
-router.post('/dev/seed', requireAuth, ctrl.seedDemoData);
-
-module.exports = router;
